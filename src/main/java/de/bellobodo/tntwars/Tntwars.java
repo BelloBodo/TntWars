82d372f9b391156a280b94cc15251f0b3a15353d
BelloBodo <pkunius@gmail.com>
2022-10-23 15:17:14 +0200

    Initial commit

---
+++ src/main/java/de/bellobodo/tntwars/Tntwars.java
@@ -0,0 +1,17 @@
+package de.bellobodo.tntwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tntwars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
