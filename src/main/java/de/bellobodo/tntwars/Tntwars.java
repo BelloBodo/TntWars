afb0e16fa8a5aa618ca78e77e181e1c836108711
BelloBodo <pkunius@gmail.com>
2022-10-23 15:25:32 +0200

    Added Shutdown Message

--- src/main/java/de/bellobodo/tntwars/Tntwars.java
+++ src/main/java/de/bellobodo/tntwars/Tntwars.java
@@ -1,5 +1,6 @@
 package de.bellobodo.tntwars;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Tntwars extends JavaPlugin {
@@ -13,5 +14,6 @@ public final class Tntwars extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        Bukkit.broadcastMessage("OFF");
     }
 }
